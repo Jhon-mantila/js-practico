6a09e6a02ccd3ab7ed629899eeef5b79d6ca6d41
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-26 22:03:04 -0500

    Funsion menu con el menuMobile

--- practica-js/main.js
+++ practica-js/main.js
@@ -1,11 +1,21 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const burgerMenu = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
+burgerMenu.addEventListener('click', toogleMobileMenu);
+
 function toogleDesktopMenu(){
-    console.log('Click');
+    console.log('Click Desktop');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    console.log('Click mobile');
+    menuMobile.classList.toggle('inactive');
 }
 
 console.log('Ingreso al archivo');
