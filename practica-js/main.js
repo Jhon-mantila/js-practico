e432c397721a21fa99657f4969f8da2414ffd1b8
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-28 20:20:53 -0500

    Agrego lista de productos con js al html y agrego listas

--- practica-js/main.js
+++ practica-js/main.js
@@ -1,15 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
 const burgerMenu = document.querySelector('.menu');
 const menuMobile = document.querySelector('.mobile-menu');
 
-
-
+const cardContainer = document.querySelector('.cards-container');
 menuEmail.addEventListener('click', toogleDesktopMenu);
 burgerMenu.addEventListener('click', toogleMobileMenu);
 menuCarritoIcon.addEventListener('click', toogleCarritoAside);
@@ -51,4 +49,73 @@ function toogleCarritoAside(){
 
 }
 
+const productList = [];
+productList.push({
+    name:'Computer',
+    precio: 120,
+    img: 'https://3ztech.com.co/wp-content/uploads/2022/11/pexels-sora-shimazaki-5926382.jpg'
+});
+productList.push({
+    name:'Computer 1',
+    precio: 220,
+    img: 'https://3ztech.com.co/wp-content/uploads/2022/11/pexels-sora-shimazaki-5926382.jpg'
+});
+productList.push({
+    name:'Computer 2',
+    precio: 330,
+    img: 'https://3ztech.com.co/wp-content/uploads/2022/11/pexels-sora-shimazaki-5926382.jpg'
+});
+/*
+<div class="cards-container">
+<div class="product-card">
+    <img src="https://3ztech.com.co/wp-content/uploads/2022/11/pexels-sora-shimazaki-5926382.jpg" alt="" class="product-img">
+    <div class="product-info">
+        <div>
+            <p>$120.00</p>
+            <p>Computer</p>
+        </div>                
+        <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+</div>
+*/
+/*recoger el array por nombre del objeto la otra opción con el in */
+renderProducts(productList);
+function renderProducts(arr){
+
+    for (let product of arr) {
+        //console.log(product.name);
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.img);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$ ' + product.precio;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.append(productPrice, productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardContainer.appendChild(productCard);
+    }
+}
 console.log('Ingreso al archivo');
